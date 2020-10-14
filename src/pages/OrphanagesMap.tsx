dd9931066012b20f326faf9c9990720420919643
vyk1 <victoriabotelho14@gmail.com>
2020-10-14 02:16:03 -0300

    Feat: Dia 1 completo

---
+++ src/pages/OrphanagesMap.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { FiPlus } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
+import { Map, TileLayer } from 'react-leaflet'
+
+import 'leaflet/dist/leaflet.css'
+import mapMarkerImg from '../images/map-marker.svg'
+import '../styles/pages/orphanages-map.css'
+
+const OrphanagesMap = () => {
+    return (
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Happy" />
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita :)</p>
+                </header>
+                <footer>
+                    <strong>Curitibanos</strong>
+                    <span>SC</span>
+                </footer>
+            </aside>
+
+            <Map
+                center={[-27.281694, -50.584492]}
+                zoom={15}
+                style={{ width: '100%', height: '100%' }}
+            >
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            </Map>
+            
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#fff" />
+            </Link>
+        </div>
+    )
+}
+
+export default OrphanagesMap
